b0201cc51e9edb3e9424aa2d80a23b275af757d1
MalyshevMS <matvey.malyshev29@gmail.com>
2026-01-27 21:45:09 +0300

    добавил класс для логера

---
+++ src/q3d/log/log.cpp
@@ -0,0 +1,24 @@
+#include <q3d/log/log.hpp>
+#include <format>
+#include <print>
+
+template <typename... Args>
+void q3d::log::info(std::string_view fmt, Args &&...message_args) {
+    std::println("q3d INFO: {}", 
+        std::vformat(fmt, std::make_format_args(std::forward<Args>(message_args)...))
+    );
+}
+
+template <typename... Args>
+void q3d::log::warn(std::string_view fmt, Args &&...message_args) {
+    std::println("q3d WARNING: {}", 
+        std::vformat(fmt, std::make_format_args(std::forward<Args>(message_args)...))
+    );
+}
+
+template <typename... Args>
+void q3d::log::error(std::string_view fmt, Args &&...message_args) {
+    std::println("q3d ERROR: {}!", 
+        std::vformat(fmt, std::make_format_args(std::forward<Args>(message_args)...))
+    );
+}
