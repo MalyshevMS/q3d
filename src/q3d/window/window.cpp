1c5a375be6bf1bbdb6d07fb7a985a4602c031385
MalyshevMS <matvey.malyshev29@gmail.com>
2026-01-27 21:42:36 +0300

    added logger class

--- src/q3d/window/window.cpp
+++ src/q3d/window/window.cpp
@@ -1,6 +1,21 @@
+#include <glad/glad.h>
 #include <q3d/window/window.hpp>
-#include <print>
+#include <q3d/log/log.hpp>
 
-void test() {
-    std::println("Hello, World!");
+q3d::Window::Window(std::string_view title, glm::vec2 size) {
+    if (!glfwInit()) {
+        log::error("glfwInit failed");
+        return;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    handle = glfwCreateWindow(size.x, size.y, title.cbegin(), nullptr, nullptr);
+
+    if (!handle) {
+        log::error("glfwCreateWindow failed");
+        return;
+    }
 }
