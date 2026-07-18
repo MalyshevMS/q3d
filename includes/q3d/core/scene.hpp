29ac47a64e0f3eba43dd3e8acdf262938662e99a
MalyshevMS <matvey.malyshev29@gmail.com>
2026-07-18 15:49:07 +0300

    сделал фабричный метод create<T> в сцене

--- includes/q3d/core/scene.hpp
+++ includes/q3d/core/scene.hpp
@@ -17,6 +17,13 @@ public:
     ptr<Object> get(std::string_view name);
     ptr<Object> operator[](std::string_view name) { return get(name); }
 
+    template<class T, class... Args>
+    ptr<T> create(std::string_view name, Args&&... args) {
+        auto obj = std::make_shared<T>(std::forward<Args>(args)...);
+        add(name, obj);
+        return obj;
+    }
+
     virtual void render() const;
 };
 
